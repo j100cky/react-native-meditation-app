0b1e664fc628689b8de575fd66cd9d658716b6c6
j100cky <jaczheng@ucdavis.edu>
2025-05-10 23:13:15 -0400

    commented index.js

--- meditationApp/app/index.js
+++ meditationApp/app/index.js
@@ -1,3 +1,5 @@
+
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Redirect } from "expo-router";
 import { useEffect, useState } from "react";
@@ -19,12 +21,17 @@ export default function Index() {
             console.error("Error checking login state:", error);
           }
           // Q: What is the purpose of using try...catch? 
+          // A: if not using try...catch, the app will crash. 
+          // After using try...catch, the remaining of the code can run. 
           setIsLoading(false);
         };
     
         checkLoginState();
       }, []);
       if (isLoading) {
+        // Q: why is this called only once but will continuously update? 
+        // A: It is not continuously checked. It just runs everytime a React 
+        // component is updated. 
             return (
               <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
                 <ActivityIndicator size="large" />
